66b8a97d6f632b5d0708b5e25b0cecab4d6e5237
daovanduc <duc07092000@gmail.com>
2020-04-21 23:50:40 +0700

    update index

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
 <?php
 include_once 'class/Stack.php';
+
+$myStack = new Stack(10);
+$myStack->push(1);
+$myStack->push(2);
+$myStack->push(3);
+$myStack->push(4);
+$myStack->push(5);
+$myStack->pop();
+$myStack->pop();
+$myStack->pop();
+$myStack->push(6);
+$myStack->push(7);
+var_dump($myStack->isEmpty());
+$myStack->pop();
+$myStack->pop();
+$myStack->pop();
+$myStack->pop();
+var_dump($myStack->isEmpty());
+$myStack->top();
